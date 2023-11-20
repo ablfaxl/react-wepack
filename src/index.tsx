568ed5f994eaeb5d0503e5ad28c3dc6f3eb26c9b
abolfazl taghavi <taghavi.abolfazl@outlook.com>
2023-11-20 18:11:06 +0330

    clear comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
-// ReactDOM.render(<App />, document.getElementById("root"));
-
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
